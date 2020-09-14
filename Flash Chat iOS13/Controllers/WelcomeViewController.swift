9b6d3b73003e676b7254026262f42594791f7b7b
macintoshhd <1234>
2020-09-14 14:24:17 +0700

    textlable animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -2,14 +2,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        var chartIndex = 0.0
+        let titles = "⚡️FlashChat"
+        for title in titles {
+            print(title);
+            print(chartIndex );
+            Timer.scheduledTimer(withTimeInterval: 0.1 * chartIndex, repeats: false){
+                (timer) in
+                self.titleLabel.text?.append(title)
+            }
+            chartIndex += 1
+        }
     }
-    
-
 }
